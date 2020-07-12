624d35adb07d2d7ae5bb187af475309199a8a6ad
airtonferreira <airton.felix@dcx.ufpb.br>
2020-07-12 14:47:40 -0300

    crate transaction model

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:daily_expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(DailyExpensesApp());
@@ -10,6 +11,21 @@ class DailyExpensesApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: '1',
+      title: 'XBOX ONE X',
+      value: 3059.00,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: '2',
+      title: 'TV SAMSUNG UHD 4K HDR 5"',
+      value: 2299.00,
+      date: DateTime.now(),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
