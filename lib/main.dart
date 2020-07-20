9bedaa0ebf6b30ce40b8910f46687e13d8a69071
airtonferreira <airton.felix@dcx.ufpb.br>
2020-07-19 23:57:46 -0300

    Form basic validation

--- lib/main.dart
+++ lib/main.dart
@@ -15,23 +15,32 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Despesas Pessoais'),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                child: Card(
-                  color: Colors.blueAccent,
-                  child: Text('Gráfico'),
-                  elevation: 5,
-                ),
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+        actions: <Widget>[IconButton(icon: Icon(Icons.add), onPressed: () {})],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                color: Colors.blueAccent,
+                child: Text('Gráfico'),
+                elevation: 5,
               ),
-              TransactionDb(),
-            ],
-          ),
-        ));
+            ),
+            TransactionDb(),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: (() {}),
+        icon: Icon(Icons.add_circle),
+        label: Text('Nova Transação'),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
