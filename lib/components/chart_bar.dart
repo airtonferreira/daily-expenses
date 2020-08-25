ccfcb56936c2df2e7c179fb7df50220078629384
airtonferreira <airton.felix@dcx.ufpb.br>
2020-08-24 22:07:36 -0300

    adjust height char

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -11,7 +11,12 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        FittedBox(child: Text('${price.toStringAsFixed(2)}')),
+        Container(
+          height: 20,
+          child: FittedBox(
+            child: Text('${price.toStringAsFixed(2)}'),
+          ),
+        ),
         SizedBox(height: 5),
         Container(
           height: 60,
